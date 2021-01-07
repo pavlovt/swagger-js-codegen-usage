8dce4cce71747107be9a28a1836aaffd3b29809a
Todor Pavlov <pavlovt@gmail.com>
2021-01-07 17:29:37 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Create TypeScript or JavaScript Api using Swagger 2.0 json
+
+- Create the json file - if you use other format than swagger 2.0 then use https://www.apimatic.io/ to convert it
+- Save the file into the project directory and change the index.js (file variable)
+- yarn && yarn serve
---
+++ blogApi.ts
@@ -0,0 +1,166 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import * as request from "superagent";
+import {
+    SuperAgentStatic
+} from "superagent";
+
+type CallbackHandler = (err: any, res ? : request.Response) => void;
+
+type Logger = {
+    log: (line: string) => any
+};
+
+/**
+ * 
+ * @class Test
+ * @param {(string)} [domainOrOptions] - The project domain.
+ */
+export default class Test {
+
+    private domain: string = "http://localhost:1337";
+    private errorHandlers: CallbackHandler[] = [];
+
+    constructor(domain ? : string, private logger ? : Logger) {
+        if (domain) {
+            this.domain = domain;
+        }
+    }
+
+    getDomain() {
+        return this.domain;
+    }
+
+    addErrorHandler(handler: CallbackHandler) {
+        this.errorHandlers.push(handler);
+    }
+
+    private request(method: string, url: string, body: any, headers: any, queryParameters: any, form: any, reject: CallbackHandler, resolve: CallbackHandler) {
+        if (this.logger) {
+            this.logger.log(`Call ${method} ${url}`);
+        }
+
+        let req = (request as SuperAgentStatic)(method, url).query(queryParameters);
+
+        Object.keys(headers).forEach(key => {
+            req.set(key, headers[key]);
+        });
+
+        if (body) {
+            req.send(body);
+        }
+
+        if (typeof(body) === 'object' && !(body.constructor.name === 'Buffer')) {
+            req.set('Content-Type', 'application/json');
+        }
+
+        if (Object.keys(form).length > 0) {
+            req.type('form');
+            req.send(form);
+        }
+
+        req.end((error, response) => {
+            if (error || !response.ok) {
+                reject(error);
+                this.errorHandlers.forEach(handler => handler(error));
+            } else {
+                resolve(response);
+            }
+        });
+    }
+
+    postsURL(parameters: {
+        $queryParameters ? : any,
+        $domain ? : string
+    }): string {
+        let queryParameters: any = {};
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/posts';
+
+        if (parameters.$queryParameters) {
+            Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+            });
+        }
+
+        let keys = Object.keys(queryParameters);
+        return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '');
+    }
+
+    /**
+     * posts
+     * @method
+     * @name Test#posts
+     */
+    posts(parameters: {
+        $queryParameters ? : any,
+        $domain ? : string
+    }): Promise < request.Response > {
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/posts';
+        let body: any;
+        let queryParameters: any = {};
+        let headers: any = {};
+        let form: any = {};
+        return new Promise((resolve, reject) => {
+            headers['Accept'] = 'application/json';
+            headers['Content-Type'] = 'application/json';
+
+            if (parameters.$queryParameters) {
+                Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                    queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+                });
+            }
+
+            this.request('GET', domain + path, body, headers, queryParameters, form, reject, resolve);
+        });
+    }
+
+    tagsURL(parameters: {
+        $queryParameters ? : any,
+        $domain ? : string
+    }): string {
+        let queryParameters: any = {};
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/tags';
+
+        if (parameters.$queryParameters) {
+            Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+            });
+        }
+
+        let keys = Object.keys(queryParameters);
+        return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '');
+    }
+
+    /**
+     * tags
+     * @method
+     * @name Test#tags
+     */
+    tags(parameters: {
+        $queryParameters ? : any,
+        $domain ? : string
+    }): Promise < request.Response > {
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        let path = '/tags';
+        let body: any;
+        let queryParameters: any = {};
+        let headers: any = {};
+        let form: any = {};
+        return new Promise((resolve, reject) => {
+            headers['Accept'] = 'application/json';
+            headers['Content-Type'] = 'application/json';
+
+            if (parameters.$queryParameters) {
+                Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                    queryParameters[parameterName] = parameters.$queryParameters[parameterName];
+                });
+            }
+
+            this.request('GET', domain + path, body, headers, queryParameters, form, reject, resolve);
+        });
+    }
+
+}
